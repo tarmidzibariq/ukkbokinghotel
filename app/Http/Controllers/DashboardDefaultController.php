f5cd362c9760511f06a60612fd35c2c83f522366
tarmidzibariq <tarmidzibariq30@gmail.com>
2022-04-12 10:14:47 +0700

    kelar riq alhamdulillahh

--- app/Http/Controllers/DashboardDefaultController.php
+++ app/Http/Controllers/DashboardDefaultController.php
@@ -72,9 +72,9 @@ class DashboardDefaultController extends Controller
         
         $triger = $request->stock - $request->quantity;
         // dd($triger);
-        $update = RoomTipe::find($request->id_room_tipe)->update([
-            'stock' => $triger,
-        ]);
+        // $update = RoomTipe::find($request->id_room_tipe)->update([
+        //     'stock' => $triger,
+        // ]);
         $update = Room::where('id_room_tipe', $request->id_room_tipe)->where('status',1)->take($request->quantity)->update([
             'status'=> 0,
         ]);
